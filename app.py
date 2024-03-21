f18b81e3599ddc25a4510389993116b803515fc8
JasonSJ <jasonjokumsen177@gmail.com>
2024-03-21 08:54:00 +0100

    Initialize repository

---
+++ app.py
@@ -0,0 +1,26 @@
+import random
+
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# A view
+@app.route("/")
+def root():
+    return redirect(url_for('add'))
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    a = 0
+    b = 0
+    c = None
+    if request.method == 'POST':
+        a = request.form['a']
+        b = request.form['b']
+        c = int(a)+int(b)
+
+    return render_template('add.html', result=c, a1=a, b1=b)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", debug=True)
